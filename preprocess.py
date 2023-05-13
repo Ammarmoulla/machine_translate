14682fc6ecd7fc655f173540d4d50df47ee027a2
Ammar moulla <ammarturing@gmail.com>
2023-05-13 13:39:13 +0300

    Function pipeline process

--- preprocess.py
+++ preprocess.py
@@ -33,23 +33,12 @@ def padding(x, length_of_pad=None, type_pad="pre"):
 
   return pad_sequences(x, maxlen=length_of_pad, padding=type_pad)
     
-if __name__ == '__main__':
+def full_process(_input, _output, length, type_pad="post"):
+  
+  seq_input, token_input = convert_text_to_tokenize(_input)
+  seq_output, token_output = convert_text_to_tokenize(_output)
 
-    show_some_sample_and_some_statics(
-        "E:\\repos\\machine_translate\\data\\small_vocab_en",
-        "E:\\repos\\machine_translate\\data\\small_vocab_fr",
-         4)
-    
-    text = [
-        "I love Nlp",
-        "I am learning Nlp",
-        "How are You",
-    ]
-    
-    sequences, token = convert_text_to_tokenize(text)
-    dictonary = token.word_index
-    print(dictonary)
-
-    for i, (sent, sequence) in enumerate(zip(text, sequences)):
-        print("sample {}: {}".format(i+1, sent))
-        print("sequence: {}".format(sequence))
+  pad_input = padding(seq_input, length_of_pad=length, type_pad=type_pad)
+  pad_output = padding(seq_output, length_of_pad=length, type_pad=type_pad)
+  
+  return pad_input, pad_output, token_input, token_output
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import numpy as np
+from preprocess import read_data
 from preprocess import show_some_sample_and_some_statics
 from preprocess import convert_text_to_tokenize
 from preprocess import padding
+from preprocess import full_process
 
 show_some_sample_and_some_statics(
         "/content/machine_translate/data/small_vocab_en",
@@ -30,3 +32,22 @@ for i, (sent, sequence, pad) in enumerate(zip(text, sequences, pad_sequences)):
   print("token and sequence: {}".format(np.array(sequence)))
   print("padding sequences:  {}".format(pad))
   print("==========================")
+
+
+en_input = read_data("/content/machine_translate/data/small_vocab_en")
+fr_output = read_data("/content/machine_translate/data/small_vocab_fr")
+length = None
+type_pad = "post"
+process_input, process_output, token_en, token_fr = full_process(en_input, fr_output, length=length, type_pad=type_pad)
+    
+maxlen_en_sequence = process_input.shape[1]
+maxlen_fr_sequence = process_output.shape[1]
+dict_en_size = len(token_en.word_index)
+dict_fr_size = len(token_fr.word_index)
+
+print('Data Preprocessed')
+print("Max English sequence length:", maxlen_en_sequence)
+print("Max French sequence length:", maxlen_fr_sequence)
+print("English vocabulary size:", dict_en_size)
+print("French vocabulary size:", dict_fr_size)
+print("==========================")
