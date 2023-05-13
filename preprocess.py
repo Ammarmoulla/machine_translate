9223a0cae449b00b9a0bd9e5e51a4cf62f5d705d
Ammar moulla <ammarturing@gmail.com>
2023-05-13 12:27:52 +0300

    new process and test

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import os
 # from keras.preprocessing.text import Tokenizer
-from keras_preprocessing.sequence import pad_sequences
+# from keras_preprocessing.sequence import pad_sequences
 
 def read_data(path_data):
 
@@ -25,10 +25,31 @@ def show_some_sample_and_some_statics(path_en, path_fr, number_sample=2):
     print("number sentence english is :", len(data_fr))
     
 def convert_text_to_tokenize(list_of_senetence):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(list_of_senetence)
+    return tokenizer.texts_to_sequences(list_of_senetence), tokenizer
+
+def padding(x, length_of_pad=None, type_pad="pre"):
+
+  return pad_sequences(x, maxlen=length_of_pad, padding=type_pad)
     
 if __name__ == '__main__':
 
     show_some_sample_and_some_statics(
-        "E:\\repos\\AIND-Capstone\\data\\small_vocab_en",
-        "E:\\repos\\AIND-Capstone\data\small_vocab_fr",
+        "E:\\repos\\machine_translate\\data\\small_vocab_en",
+        "E:\\repos\\machine_translate\\data\\small_vocab_fr",
          4)
+    
+    text = [
+        "I love Nlp",
+        "I am learning Nlp",
+        "How are You",
+    ]
+    
+    sequences, token = convert_text_to_tokenize(text)
+    dictonary = token.word_index
+    print(dictonary)
+
+    for i, (sent, sequence) in enumerate(zip(text, sequences)):
+        print("sample {}: {}".format(i+1, sent))
+        print("sequence: {}".format(sequence))
---
+++ test.py
@@ -0,0 +1,33 @@
+import numpy as np
+from preprocess import show_some_sample_and_some_statics
+from preprocess import convert_text_to_tokenize
+from preprocess import padding
+
+
+show_some_sample_and_some_statics(
+        "E:\\repos\\machine_translate\\data\\small_vocab_en",
+        "E:\\repos\\machine_translate\\data\\small_vocab_fr",
+         4)
+
+text = [
+        "I love Nlp",
+        "I am learning Nlp",
+        "How are You",
+    ]
+    
+#token_and_sequences
+sequences, token = convert_text_to_tokenize(text)
+dictonary = token.word_index
+print("==========================")
+print("index for each word")
+print(dictonary)
+print("==========================")
+
+#padding
+pad_sequences = padding(sequences)
+
+for i, (sent, sequence, pad) in enumerate(zip(text, sequences, pad_sequences)):
+  print("sample {}: {}".format(i+1, sent))
+  print("token and sequence: {}".format(np.array(sequence)))
+  print("padding sequences:  {}".format(pad))
+  print("==========================")
