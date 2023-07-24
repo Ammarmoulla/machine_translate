096a69ccc9128ac363d78de136f38b9af6f05187
Ammar moulla <ammarturing@gmail.com>
2023-07-24 13:36:13 +0300

    some expriment to ci/cd :construction_worker:

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #python3 test.py
 # cd fastapi
 # sh start.sh
-
+apt-get install graphviz -y
 python3 test_preprocess.py
 python3 train.py --config_path "train.yaml"
 python3 inference.py --model_path "/content/machine_translate/outputs/model_Lstm_Embd.h5" --text "new jersey is sometimes quiet"
