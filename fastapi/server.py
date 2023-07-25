cd3907058a0666990269098cc90c1f621a72e7ff
Ammar moulla <ammarturing@gmail.com>
2023-07-25 22:07:55 +0300

    :wrench: limit allowed requests for ngrok

--- fastapi/server.py
+++ fastapi/server.py
@@ -10,7 +10,17 @@ port = 8000
 if __name__ == '__main__':
 
     ngrok.set_auth_token("2ST8bvXhOYVVsUKv1qe1jOnqgNW_5N8Pt75i32ijBJh3NDcNJ")
-    ngrok_tunnel = ngrok.connect(8000)
-    print('Public URL:', ngrok_tunnel.public_url)
+    public_url = ngrok.connect(8000).public_url
+    print("Public URL:", public_url)
     nest_asyncio.apply()
     uvicorn.run(app, port=8000)
+    
+    allowed_requests = 2
+    request_count = 0
+    while True:
+        request_count += 1
+
+        if request_count > allowed_requests:
+            ngrok.disconnect(public_url)
+            ngrok.kill()
+            break
